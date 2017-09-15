251cb10955cb51250cc79b8e0b6303ad76fe64eb
nyg <nyg@users.noreply.github.com>
2017-09-15 22:15:53 +0200

    Add posibility to use ServerSocketChannel instead of ServerSocket.

--- src/main/java/edu/self/server/Server.java
+++ src/main/java/edu/self/server/Server.java
@@ -3,20 +3,31 @@ package edu.self.server;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
 import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 
 public class Server {
 
+    // we always return the same HTTP response
+    private static final String RESPONSE_CONTENT = "Hello World!";
+    private static final String HTTP_RESPONSE = "HTTP/1.0 200 OK\nContent-Type: text/plain\nContent-Length: " + RESPONSE_CONTENT.length() + "\n\n" + RESPONSE_CONTENT;
+    private static final ByteBuffer HTTP_RESPONSE_BUFFER = ByteBuffer.wrap(HTTP_RESPONSE.getBytes());
+
     public static void main(String args[]) throws Exception {
 
-        if (args.length != 3) {
-            System.err.println("Arguments must be: [IP version] [IP address] [port], e.g. 6 ::1 8080");
+        if (args.length != 3 && args.length != 4) {
+            System.err.println("Arguments must be: [IP version] [IP address] [port], e.g. 6 ::1 8080.");
+            System.err.println("Use a fourth argument to use ServerSocketChannel instead of ServerSocket.");
             System.exit(-1);
         }
 
+        // get IP address
         InetAddress ip;
         if (args[0].equals("4")) {
             ip = InetAddress.getByName(args[1]);
@@ -28,29 +39,60 @@ public class Server {
             ip = Inet6Address.getByName(args[1]);
         }
 
+        // get port number
         int port = Integer.parseInt(args[2]);
-        System.out.println("IP: " + ip.toString() + ", port: " + port);
+        System.out.println("Listening to " + ip.toString() + " on port " + port);
 
-        try (ServerSocket serverSocket = new ServerSocket(port, -1, ip)) {
+        if (args.length == 3) {
 
-            String line;
-            String content = "Hello World!";
-            String response = "HTTP/1.0 200 OK\nContent-Type: text/plain\nContent-Length: " + content.length() + "\n\n" + content;
+            try (ServerSocket serverSocket = new ServerSocket(port, -1, ip)) {
 
-            while (true) {
+                while (true) {
 
-                Socket socket = serverSocket.accept();
+                    try (Socket socket = serverSocket.accept()) {
 
-                // read request - optional
-                System.out.println("--");
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                while ((line = in.readLine()) != null && !line.isEmpty()) {
-                    System.out.println(line);
+                        // read request
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        String line;
+
+                        System.out.println("\n---");
+                        while ((line = in.readLine()) != null && !line.isEmpty()) {
+                            System.out.println(line);
+                        }
+
+                        // write response
+                        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                        out.writeBytes(HTTP_RESPONSE);
+                    }
                 }
+            }
+        }
+        else {
+            try (ServerSocketChannel channel = ServerSocketChannel.open().bind(new InetSocketAddress(ip, port))) {
 
-                // always return HTTP response
-                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-                out.writeBytes(response);
+                while (true) {
+
+                    try (SocketChannel socket = channel.accept()) {
+
+                        // read request
+                        ByteBuffer requestBuffer = ByteBuffer.allocate(1000);
+                        StringBuilder request = new StringBuilder();
+
+                        do {
+                            requestBuffer.clear();
+                            socket.read(requestBuffer);
+                            request.append(new String(requestBuffer.array()).trim());
+                        }
+                        while (requestBuffer.remaining() == 0);
+
+                        System.out.println("\n--- length: " + request.length());
+                        System.out.println(request.toString());
+
+                        // write response
+                        HTTP_RESPONSE_BUFFER.rewind();
+                        socket.write(HTTP_RESPONSE_BUFFER);
+                    }
+                }
             }
         }
     }
